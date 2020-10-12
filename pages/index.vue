d00f06e0cefde93fb6595cbdffb6d731f3818ba0
hishinuma616 <dnbwoodleaves3616@gmail.com>
2020-10-12 09:20:39 +0900

    サンプル追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="l-inner">
     <h2 class="c-heading__primary">インデックス</h2>
+    <app-input :value="text"
+      @input="onChangeInput"
+      name="インプット"
+      input-type="text" />
+    {{ text }}
   </div>
 </template>
+
+<script>
+import AppInput from '~/components/atoms/AppInput.vue';
+
+export default {
+  components: {
+    AppInput,
+  },
+  data() {
+    return {
+      text: '',
+    }
+  },
+  methods: {
+    onChangeInput(value) {
+       this.text = value;
+    },
+  },
+}
+</script>
